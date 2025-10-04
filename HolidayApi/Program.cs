587de4dadf906788d36219dba8f76a7eb2a4c9fd
Divyabharathi76 <divyashankar1964@gmail.com>
2025-10-04 16:41:50 +0200

    Add files via upload

--- HolidayApi/Program.cs
+++ HolidayApi/Program.cs
@@ -1,1 +1,35 @@
+using HolidayApi.Data;
+using HolidayApi.Services;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register DbContext 
+builder.Services.AddDbContext<HolidaysDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<HolidayService>();
+
+var app = builder.Build();
+
+// Configure middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
